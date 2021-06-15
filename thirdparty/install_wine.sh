abe8b8695903df830c34cc871a15c92177515cb1
chLi <565353780@qq.com>
2021-06-15 17:33:46 +0800

    now can use WeChat on linux

--- thirdparty/install_wine.sh
+++ thirdparty/install_wine.sh
@@ -7,7 +7,7 @@ rm winehq.key
 sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
 sudo apt update
 sudo apt install --install-recommends winehq-stable -y
-sudo apt install winetricks libncurses5:i386 -y
+sudo apt install winetricks libncurses5:i386 libnvidia-gl-460:i386 -y
 wget http://mirrors.ustc.edu.cn/wine/wine/wine-mono/6.2.0/wine-mono-6.2.0-x86.msi
 wget http://mirrors.ustc.edu.cn/wine/wine/wine-gecko/2.47.2/wine-gecko-2.47.2-x86.msi
 wget http://mirrors.ustc.edu.cn/wine/wine/wine-gecko/2.47.2/wine-gecko-2.47.2-x86_64.msi
@@ -16,4 +16,6 @@ wine start /i wine-gecko-2.47.2-x86.msi
 wine start /i wine-gecko-2.47.2-x86_64.msi
 
 winetricks riched20 richtx32 corefonts cjkfonts
+
+sudo apt install 
 
