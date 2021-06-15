4d505066d2daad339030710a32719cb3bdb2a783
chLi <565353780@qq.com>
2021-06-15 16:39:51 +0800

    update install bash file

--- install.sh
+++ install.sh
@@ -3,5 +3,9 @@ bash my_install_sh/install_zsh.sh
 bash my_install_sh/install_nvim.sh
 bash my_install_sh/install_thirdparty.sh
 bash my_install_sh/install_ssh.sh
-bash my_install_sh/install_wine.sh
+
+bash thirdparty/install_wine.sh
+bash thirdparty/install_anaconda.sh
+bash thirdparty/install_edge.sh
+bash thirdparty/install_ros.sh
 
