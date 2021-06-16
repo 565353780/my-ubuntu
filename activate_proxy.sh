1de874650dc093c48396b80767a9ccbdfd888b8f
chLi <565353780@qq.com>
2021-06-16 15:30:52 +0800

    add pip support

--- activate_proxy.sh
+++ activate_proxy.sh
@@ -50,3 +50,7 @@ cat my_config_file/my_bashrc_append >> ~/.bashrc
 cp my_config_file/my_gitconfig ~/.gitconfig
 
 cp my_config_file/my_xmodmap ~/.Xmodmap
+
+mkdir ~/.pip
+cp my_config_file/my_pip_conf ~/.pip/pip.conf
+
